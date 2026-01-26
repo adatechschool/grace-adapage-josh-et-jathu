d47abed26ff7650cd5f728600afa061dfaaad5c6
jw-rose <220035988+jw-rose@users.noreply.github.com>
2026-01-26 16:03:35 +0100

    404 page  updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import { Home } from './pages/Home';
 import { Features } from './pages/Features';
 import { Timeline } from './pages/Timeline';
 import { Testimonial } from './pages/Testimonial';
+import NotFoundPage from './pages/NotFoundPage';
 
 
 function App() {
   return (
     <>
-    <Home  />
+    <Home />
     <Features />
     <Timeline />
     <Testimonial />
+    <NotFoundPage />
 
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,13 @@ import { Home } from './pages/Home';
 import { Features } from './pages/Features';
 import { Timeline } from './pages/Timeline';
 import { Testimonial } from './pages/Testimonial';
+import  NotFoundPage  from './pages/NotFoundPage';
 
 const router = createBrowserRouter([
   {
   path: '/',
   element: <Home />,
+  errorElement: <NotFoundPage />,
 
 },
 
