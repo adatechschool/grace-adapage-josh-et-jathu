021abcb3e015f71aa85a2d4e44d70409a11f5a2f
teizred <91955720+teizred@users.noreply.github.com>
2026-01-26 14:14:53 +0100

    push with pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import './App.css'
+import { Outlet } from 'react-router-dom';
+import { Navbar } from './components/layout/Navbar';
+import './styles/globals.css';
 
 function App() {
-
   return (
     <>
-    <h1>Adapage</h1>
-
-    <p>Test run</p>
-
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
