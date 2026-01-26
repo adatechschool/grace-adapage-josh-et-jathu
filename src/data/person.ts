bfa70e0e29528c350f977f47081095be6c97dc7e
teizred <91955720+teizred@users.noreply.github.com>
2026-01-26 15:28:54 +0100

    feat: Define `Person` type structure and populate initial person data for Mia Goth.

--- src/data/person.ts
+++ src/data/person.ts
@@ -1,5 +1,43 @@
 import type { Person } from "../types/person";
 
 export const person: Person = {
+    name: "Mia Goth",
+    image: "",
+    description:"Mia Goth est une actrice  britannique connue pour ses rôles marquants dans le cinéma d'horreur et d'auteur.",
+    infoCards: [
+        {title: "Nationalité", value: "Britannique"},
+        {title: "Métier", value:"Actrice"},
+        {title: "Signe Astrologique", value:"Scorpion"},
+        {title: "Date de naissance", value:"1999-10-25"},
+        {title: "Qualité", value:"Intensité"}
+    ],
+    timeline: [
+        {
+            year: "2018",
+            title: "Suspiria",
+            description: "Rôle remarqué dans le film de Luca Guadagnino."
+        },
+        {
+            year: "2022",
+            title: "X / Pearl",
+            description: "Performance acclamée dans les films de Ti West.",
+            quote: "Pearl is a character who wants to be seen.",
+        },
+    ],
+    quotes: [
+        {
+            text: "I’m drawn to characters who are misunderstood.",
+            source: "Interview"
+        },
+        {
+            text: "I like characters that are pushed to extremes.",
+            source: "Interview"
+        },
+        {
+            text: "I’m not interested in being likable, I’m interested in being honest.",
+            source: "Interview"
+        }
+    ]
+
 
 };
