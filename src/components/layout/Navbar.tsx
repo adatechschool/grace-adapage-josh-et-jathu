c88921923898810b4dfd42e68bdfe6c3d5beef3a
teizred <91955720+teizred@users.noreply.github.com>
2026-01-27 15:52:08 +0100

    refactor: simplify routing initialization and update navbar styling and structure.

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,26 +2,25 @@ import { Link } from "react-router-dom"
 
 export default function Navbar() {
   return (
-    <nav className="nav">
-      <Link to="/" className="home-placeholder">Home</Link>
+    <nav className="bg-neutral-900 text-neutral-300 py-3">
 
-      <ul className="flex items-center gap-x-2 p-1 font-medium">
+      <ul className="flex flex-1 gap-6 justify-center">
+        <CustomLink to= "/">Home</CustomLink>
         <CustomLink to= "/Features">Features</CustomLink>
         <CustomLink to="/Timeline">Timeline</CustomLink>
         <CustomLink to="/Testimonial">Testimonial</CustomLink>
       </ul>
-    </nav>
+    </nav>  
   );
 }
 
 
-function CustomLink({ to, children }) {
+function CustomLink({ to, children }: { to: string; children: React.ReactNode }) {
     const path = window.location.pathname
   
     return (
     <li className={path === to ? "active" : ""}>
-      <Link to={to}>{children}
-      </Link>
+      <Link to={to}>{children}</Link>
     </li>
   );
 }
