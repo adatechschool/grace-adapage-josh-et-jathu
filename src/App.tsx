72d632f14df14be2d9906e68fdf474eb4518db28
jw-rose <220035988+jw-rose@users.noreply.github.com>
2026-01-27 11:49:50 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,24 @@ import { Home } from './pages/Home';
 import { Features } from './pages/Features';
 import { Timeline } from './pages/Timeline';
 import { Testimonial } from './pages/Testimonial';
+import  Navbar  from './components/layout/Navbar'
 import NotFoundPage from './pages/NotFoundPage';
 
 
 function App() {
+
+
   return (
     <>
+    
+    
+    <Navbar />
     <Home />
     <Features />
     <Timeline />
     <Testimonial />
     <NotFoundPage />
+    
 
     </>
   );
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,2 +1,25 @@
+import { NavLink } from "react-router-dom";
+
+export default  function Navbar() {
+  return (
+    <nav className="navBar">
+      <NavLink to="/" end>
+        Home
+      </NavLink>
+
+      <NavLink to="/features">
+        Features
+      </NavLink>
+
+      <NavLink to="/timeline">
+        Timeline
+      </NavLink>
+
+      <NavLink to="/testimonials">
+        Testimonials
+      </NavLink>
+    </nav>
+  );
+};
 
 
