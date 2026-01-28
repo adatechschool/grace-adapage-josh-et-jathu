59bfc392f2dfd79a434ab9ab2e0f84386ac2feb9
jw-rose <220035988+jw-rose@users.noreply.github.com>
2026-01-28 14:16:39 +0100

    push test

--- src/pages/Testimonial.tsx
+++ src/pages/Testimonial.tsx
@@ -1,9 +1,30 @@
-
+import { person} from "../data/person"
 export const  Testimonial = () => {
-    return <>
+    return (
+
+    <main className="">
+
+        <h1>Teigmoinage</h1>
+
+        <section className=" grid grid-cols-1 bg-red-900 dark:bg-gray-900 text-gray-900 p-12">
+
+            <div className="flex flex-hor items-center gap-8 w-full max-w-2xl mx-auto bg-white">
+                {person.quotes.map((quote) => (
+                <div key={quote.text} className="bg-blue-500 rounded  border-gray-500 transition-shadow hover:scale-105 duration-500 ease-out s ">
+                    <span className="flex-1 font-extrabold ">
+                        {quote.text}
+                    </span>
+                    <span>
+                        {quote.source}
+                    </span>
+                    </div>
+                ))}
+
+                
+            </div>
 
 
-    <h1>Testimonial</h1>
-    
-    </>
-}
+        </section>
+    </main>
+    );
+};
