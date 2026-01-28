7737d05a58e2363aa754eb917a4bc7401a7807b3
teizred <91955720+teizred@users.noreply.github.com>
2026-01-28 11:36:11 +0100

    children reactnode customlink

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -5,27 +5,17 @@ export default function Navbar() {
     <nav className="bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white py-3">
 
       <ul className="flex flex-1 gap-6 justify-center">
-<<<<<<< HEAD
-        <CustomLink to= "/">Home</CustomLink>
-        <CustomLink to= "/Features">Features</CustomLink>
-        <CustomLink to="/Timeline">Timeline</CustomLink>
-        <CustomLink to="/Testimonial">Testimonial</CustomLink>
-        
-        
-
-=======
         <CustomLink to= "/">Acceuil</CustomLink>
         <CustomLink to= "/Features">Caractéristiques</CustomLink>
         <CustomLink to="/Timeline">Chronologie</CustomLink>
         <CustomLink to="/Testimonial">Témoignages</CustomLink>
->>>>>>> e4d789588c5f9c07be2f5bf4eef2c4380304966e
       </ul>
     </nav>  
   );
 }
 
 
-function CustomLink({ to, children }) {
+function CustomLink({ to, children }: { to: string; children: React.ReactNode }) {
    const resolvedPath = useResolvedPath(to)
    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
   
